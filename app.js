7ae21e8491019300fe0b93c9fbdbff465bd0805b
PlayeredLC <playered.lc@gmail.com>
2021-01-03 21:57:12 -0300

    Added input to receive city name dinamically.

--- app.js
+++ app.js
@@ -1,37 +1,49 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.use(bodyParser.urlencoded({extended: true}));
 
-  //static api parameter as Porto Alegre
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Porto%20Alegre&appid=5e92290d879a5d4bd682287d75519d87&units=metric";
+app.get("/", (req, res) => {
+  res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/", (req, res) => { //deal with post request coming from the browser (input element)
+  
+  var query = req.body.cityName;
+  var unitSystem = "metric"; 
+  var apiKey = "5e92290d879a5d4bd682287d75519d87";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apiKey +"&units=" + unitSystem;
+  
   https.get(url, (response) => { //request data from the API Server (external server)
+    
     response.on("data", (d) => { //when data is received (hex format)
       resData = JSON.parse(d);  
-    
-      var imgURL= "http://openweathermap.org/img/wn/"+ resData.weather[0].icon +"@2x.png"  
-      var displayTemp = "Temperature in "+resData.name+" is "+Math.floor(resData.main.temp)+" °C."
-      var displayDescript = ((resData.weather[0].description).slice(0,1)).toUpperCase() + (resData.weather[0].description).slice(1,(resData.weather[0].description).length);
-      
-      //SEND BACK TO BROWSER
-      res.type("html");
-      
-      res.write("<h1>");
-      res.write(displayTemp + " ");
-      res.write(displayDescript + ".");
-      res.write("</h1>");
-      res.write("<img src='"+imgURL+"' alt='Weather-Image'>")
-      res.send();
+      if(resData.cod == 200){
+        var imgURL= "http://openweathermap.org/img/wn/"+ resData.weather[0].icon +"@2x.png"  
+        var displayTemp = "Temperature in "+resData.name+" is "+Math.floor(resData.main.temp)+" °C."
+        var displayDescript = ((resData.weather[0].description).slice(0,1)).toUpperCase() + (resData.weather[0].description).slice(1,(resData.weather[0].description).length);
+        
+        //SEND BACK TO BROWSER
+        res.type("html");
+        
+        res.write("<h1>");
+        res.write(displayTemp + " ");
+        res.write(displayDescript + ".");
+        res.write("</h1>");
+        res.write("<img src='"+imgURL+"' alt='Weather-Image'>")
+        res.send();
+      }else{
+        res.send("Error "+resData.cod+". Please reload the page.");
+      }
     
     });
   
   });
 
 });
-
-
 app.listen(port, () => {
   console.log("started at port "+port);
 });
