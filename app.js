6ad7b2184bc9df060d3b44caf893a05964b50eff
PlayeredLC <playered.lc@gmail.com>
2021-01-20 18:30:57 -0300

    Get data from OpenWeather via their API.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const https = require('https');
 const bodyParser = require('body-parser');
+const { response } = require('express');
 
 const app = express();
 const port = 3000;
@@ -17,36 +18,35 @@ app.get('/', (req, res) => {
   });
 });
 
-// app.post('/', (req, res) => { //deal with post request coming from the browser (input element)
-  
-//   var query = req.body.cityName;
-//   var unitSystem = 'metric'; 
-//   var apiKey = process.env.API_KEY;
-//   const url = 'https://api.openweathermap.org/data/2.5/weather?q='+ query +'&appid='+ apiKey +'&units=' + unitSystem;
-  
-//   https.get(url, (response) => { //request data from the API Server (external server)
-    
-//     response.on('data', (d) => { //when data is received (hex format)
-//       resData = JSON.parse(d);  
-//       if(resData.cod == 200){
-//         var imgURL= 'http://openweathermap.org/img/wn/'+ resData.weather[0].icon +'@2x.png'  
-//         var displayTemp = 'Temperature in '+resData.name+' is '+Math.floor(resData.main.temp)+' °C.'
-//         var displayDescript = ((resData.weather[0].description).slice(0,1)).toUpperCase() + (resData.weather[0].description).slice(1,(resData.weather[0].description).length);
-        
-//         //SEND BACK TO BROWSER
-//         res.type('html');        
-//         res.write('<h1>');
-//         res.write(displayTemp + ' ');
-//         res.write(displayDescript + '.');
-//         res.write('</h1>');
-//         res.write('<img src=' +imgURL+ ' alt=Weather-Image>');
-//         res.send();
-//       }else{
-//         res.send('Error '+resData.cod+'. Please reload the page.');
-//       }   
-//     });
-//   });
-// });
+app.post('/', (req, res) => {
+  const units = 'metric';
+  const currentURL = 'https://api.openweathermap.org/data/2.5/weather?q='+ req.body.cityName +'&appid='+ process.env.API_KEY +'&units=' + units;
+  const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q='+ req.body.cityName +'&appid='+ process.env.API_KEY + '&units=' +units;
+
+  https.get(currentURL, (response) => {
+    response.on('data', (currData) => {
+      const currentData = JSON.parse(currData);
+      if(currentData.cod == 200){
+        console.log(currentData);
+      }
+      else{
+        console.log(currentData.cod);
+      }
+    })
+  });
+  https.get(forecastURL, (response) => {
+    response.on('data', (foreData) => {
+      const forecastData = JSON.parse(foreData);
+      if(forecastData.cod == 200){
+        console.log(forecastData);
+      }
+      else{
+        console.log(forecastData.cod);
+      }
+    });
+  });
+
+});
 
 app.listen(port, () => {
   console.log('started at port '+port);
