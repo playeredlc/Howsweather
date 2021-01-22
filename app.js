39a4e6f0dc39de7279016a9bd81887e7fc64025f
PlayeredLC <playered.lc@gmail.com>
2021-01-22 20:07:12 -0300

    Display forecast information.

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const bodyParser = require('body-parser');
 const { response } = require('express');
 
 var currentWeatherData = {};
-const forecastWeatherData = {};
+const forecastWeatherData = new Array();
 const graphicsData = new Array();
 
-var fakeData = {
+// FAKE DATA FOR TSTS.
+const fakeCurrentData = {
   description: "Broken clouds",
   temp: 34,
   feelsLike: 38,
@@ -21,6 +22,78 @@ var fakeData = {
   iconURL: 'http://openweathermap.org/img/wn/04n@4x.png'
 };
 
+const fakeGraphicsData = [
+  [28],
+  [25, 24, 22, 22, 27, 31, 32, 30],
+  [26, 25, 24, 24, 28, 34, 32, 31],
+  [28, 25, 25, 24, 28, 35, 35, 31],
+  [29, 27, 25, 24, 28, 34, 34, 32],
+  [29, 27, 26, 26, 30, 34, 33]
+];
+
+const fakeForecastData = [
+  {
+    description: 'clear sky',
+    temp: 27,
+    tempMin: 27,
+    tempMax: 27,
+    humidity: 60,
+    windSpeed: 1.66,
+    pop: 0,
+    location: 'Porto Alegre',
+    iconURL: 'http://openweathermap.org/img/wn/01d@4x.png',
+    date: 'Sat, Jan 23'
+  },
+  {
+    description: 'clear sky',
+    temp: 28,
+    tempMin: 28,
+    tempMax: 28,
+    humidity: 57,
+    windSpeed: 2.85,
+    pop: 0.11,
+    location: 'Porto Alegre',
+    iconURL: 'http://openweathermap.org/img/wn/01d@4x.png',
+    date: 'Sun, Jan 24'
+  },
+  {
+    description: 'clear sky',
+    temp: 28,
+    tempMin: 28,
+    tempMax: 28,
+    humidity: 55,
+    windSpeed: 2.41,
+    pop: 0,
+    location: 'Porto Alegre',
+    iconURL: 'http://openweathermap.org/img/wn/01d@4x.png',
+    date: 'Mon, Jan 25'
+  },
+  {
+    description: 'clear sky',
+    temp: 28,
+    tempMin: 28,
+    tempMax: 28,
+    humidity: 57,
+    windSpeed: 3.42,
+    pop: 0,
+    location: 'Porto Alegre',
+    iconURL: 'http://openweathermap.org/img/wn/01d@4x.png',
+    date: 'Tue, Jan 26'
+  },
+  {
+    description: 'broken clouds',
+    temp: 30,
+    tempMin: 30,
+    tempMax: 30,
+    humidity: 54,
+    windSpeed: 4.61,
+    pop: 0.03,
+    location: 'Porto Alegre',
+    iconURL: 'http://openweathermap.org/img/wn/04d@4x.png',
+    date: 'Wed, Jan 27'
+  }
+];
+
 const app = express();
 const port = 3000;
 
@@ -30,8 +103,9 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index', {
-    currentData: 'This is the date on current weather.',
-    forecastData: 'This is the on the forecast.'
+    currentData: fakeCurrentData,
+    forecastData: fakeForecastData,
+    graphicsData: fakeGraphicsData
   });
 });
 
@@ -40,78 +114,80 @@ app.post('/', (req, res) => {
   const currentURL = 'https://api.openweathermap.org/data/2.5/weather?q='+ req.body.cityName +'&appid='+ process.env.API_KEY +'&units=' + units;
   const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q='+ req.body.cityName +'&appid='+ process.env.API_KEY + '&units=' +units;
 
-  // REQUEST ON CURRENT DATA
-  https.get(currentURL, (response) => {
-    response.on('data', (currData) => {
-      const currentData = JSON.parse(currData);
-      if(currentData.cod == 200){
-        const currentDtDate = new Date(currentData.dt*1000);
-        currentWeatherData = {
-          description: currentData.weather[0].description,
-          temp: Math.round(currentData.main.temp),
-          feelsLike: Math.round(currentData.main.feels_like),
-          tempMin: Math.round(currentData.main.temp_min),
-          tempMax: Math.round(currentData.main.temp_max),
-          humidity: currentData.main.humidity,
-          windSpeed: currentData.wind.speed,
-          // pop: currentData.pop,
-          location: currentData.name,
-          iconURL:'http://openweathermap.org/img/wn/'+ currentData.weather[0].icon +'@4x.png',
-          date: currentDtDate.toLocaleString("en-US", {weekday: 'long', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit'})
-        };
-        console.log(currentWeatherData);
-      }
-        else{
-        console.log(currentData.cod);
-      }
-    })
-  });
+  // // REQUEST ON CURRENT DATA
+  // https.get(currentURL, (response) => {
+  //   response.on('data', (currData) => {
+  //     const currentData = JSON.parse(currData);
+  //     if(currentData.cod == 200){
+  //       const currentDtDate = new Date(currentData.dt*1000);
+  //       currentWeatherData = {
+  //         description: currentData.weather[0].description,
+  //         temp: Math.round(currentData.main.temp),
+  //         feelsLike: Math.round(currentData.main.feels_like),
+  //         tempMin: Math.round(currentData.main.temp_min),
+  //         tempMax: Math.round(currentData.main.temp_max),
+  //         humidity: currentData.main.humidity,
+  //         windSpeed: currentData.wind.speed,
+  //         location: currentData.name,
+  //         iconURL:'http://openweathermap.org/img/wn/'+ currentData.weather[0].icon +'@4x.png',
+  //         date: currentDtDate.toLocaleString("en-US", {weekday: 'long', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit'})
+  //       };
+  //       console.log(currentWeatherData);
+  //     }
+  //       else{
+  //       console.log(currentData.cod);
+  //     }
+  //   })
+  // });
 
-  // REQUEST ON FORECAST DATA
-  https.get(forecastURL, (response) => {
-    response.on('data', (foreData) => {
-      const forecastData = JSON.parse(foreData);
-      if(forecastData.cod == 200){    
-        var day=0;
-        graphicsData[day] = new Array();
-        forecastData.list.forEach(element => {          
-          //GENERAL INFORMATION FOR EACH DAY
-          if(element.dt_txt.slice(11,13) === '12' && (forecastWeatherData[0] !== null)){
-            const forecastDtDate = new Date(element.dt*1000);
-            forecastWeatherData[day] = {
-              description: element.weather[0].description,
-              temp: Math.round(element.main.temp),
-              tempMin: Math.round(element.main.temp_min),
-              tempMax: Math.round(element.main.temp_max),
-              humidity: element.main.humidity,
-              windSpeed: element.wind.speed,
-              pop: element.pop,
-              location: forecastData.city.name,
-              iconURL: 'http://openweathermap.org/img/wn/'+ element.weather[0].icon + '@4x.png',
-              date: forecastDtDate.toLocaleString("en-US", {weekday: 'short', month: 'short', day: '2-digit'})
-            };
-          }
-          // 3-HOURLY TEMPERATURE FOR EACH DAY
-          if(element.dt_txt.slice(11,13) === '00' && (graphicsData[0][0] !== null)){
-            day++;
-            graphicsData[day] = new Array();
-          }
-          graphicsData[day].push(Math.round(element.main.temp));
-        });
-        console.log(forecastWeatherData);
-        console.log(graphicsData);
-      }
-      else{
-        console.log(forecastData.cod);
-      }
-    });
-  });
-
+  // // REQUEST ON FORECAST DATA
+  // https.get(forecastURL, (response) => {
+  //   response.on('data', (foreData) => {
+  //     const forecastData = JSON.parse(foreData);
+  //     if(forecastData.cod == 200){    
+  //       var day=0;
+  //       graphicsData[day] = new Array();
+  //       forecastData.list.forEach(element => {          
+  //         //GENERAL INFORMATION FOR EACH DAY
+  //         if(element.dt_txt.slice(11,13) === '12' && (forecastWeatherData[0] !== null)){
+  //           const forecastDtDate = new Date(element.dt*1000);
+  //           forecastWeatherData[day-1] = {
+  //             description: element.weather[0].description,
+  //             temp: Math.round(element.main.temp),
+  //             tempMin: Math.round(element.main.temp_min),
+  //             tempMax: Math.round(element.main.temp_max),
+  //             humidity: element.main.humidity,
+  //             windSpeed: element.wind.speed,
+  //             pop: element.pop,
+  //             location: forecastData.city.name,
+  //             iconURL: 'http://openweathermap.org/img/wn/'+ element.weather[0].icon + '@4x.png',
+  //             date: forecastDtDate.toLocaleString("en-US", {weekday: 'short', month: 'short', day: '2-digit'})
+  //           };
+  //         }
+  //         // 3-HOURLY TEMPERATURE FOR EACH DAY
+  //         if(element.dt_txt.slice(11,13) === '00' && (graphicsData[0][0] !== null)){
+  //           day++;
+  //           graphicsData[day] = new Array();
+  //         }
+  //         graphicsData[day].push(Math.round(element.main.temp));
+  //       });
+  //       console.log(forecastWeatherData);
+  //       console.log(graphicsData);
+  //     }
+  //     else{
+  //       console.log(forecastData.cod);
+  //     }
+  //   });
+  // });
+  
+  console.log(fakeCurrentData);
+  console.log(fakeForecastData);
+  console.log(fakeGraphicsData);
   res.render('index', {
-    currentData: fakeData,
-    forecastData: 'forecast data.'
+    currentData: fakeCurrentData,
+    forecastData: fakeForecastData,
+    graphicsData: fakeGraphicsData
   });
-
 });
 
 app.listen(port, () => {
