a2cf05f9386f373f537d5b1928ffc531c97d5266
PlayeredLC <playered.lc@gmail.com>
2021-01-23 17:02:06 -0300

    Display temperature chart with real data.

--- public/script/script.js
+++ public/script/script.js
@@ -1,10 +1,13 @@
 ctx = document.getElementById('myChart').getContext('2d');
 
+myData = document.getElementById('canvasDay0').dataset.graphics;
+myData = myData.split(',').map(x=>Number(x));
+
 const chartData = {
   labels: ['00:00', '03:00', '06:00', '09:00', '12:00', '15:00', '18:00', '21:00'],
   datasets: [{
     label: 'Temperature',
-    data: [1,2,3,4,5,6,7,8], //test only
+    data: myData,
     backgroundColor: 'rgba(127, 127, 0, 0.5)',
     borderColor:  'rgba(127, 127, 0, 1)',
     borderWidth: 1
