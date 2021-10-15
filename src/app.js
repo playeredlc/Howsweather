d3634b5b2eb782e7fc6253dde8e4be027b2f0e7a
edlcorrea <playered.lc@gmail.com>
2021-10-15 01:29:56 -0300

    [refactoring] Remove body-parser dependency due to deprecation. Using express.json instead.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ type: 'application/vnd.api+json' }));
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
