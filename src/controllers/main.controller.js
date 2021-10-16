c04df95c16b2d21bd9c29d901af09a458073de0a
edlcorrea <playered.lc@gmail.com>
2021-10-15 23:30:51 -0300

    [refactoring] Rewrite controller accordingly to the new structure.

--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -1,33 +1,24 @@
-const getData = require(__dirname + '/../get-data');
-const config = require(__dirname + '/../config/config');
+const WeatherInformation = require(__dirname + '/../entities/WeatherInformation');
+const ForecastWeatherInfo = require(__dirname + '/../entities/ForecastWeatherInfo');
+
+const openWeatherProvider = require(__dirname + '/../providers/openWeatherProvider');
 
 exports.showInitialScreen = async (req, res) => {
     res.render('index');
 };
 
-exports.showWeather = async(req, res) => {
-	const currentURL = config.api.getWeatherURL(req.body.cityName);
-	const forecastURL = config.api.getForecastURL(req.body.cityName);
+exports.showWeather = async (req, res) => {
 	
-	getData.getCurrentWeatherData(currentURL, (err, result) => {
-		if(err){
-			console.log(err);
-			res.redirect('/');
-		}else{
-			var myWeatherData = result;
-			getData.getForecastWeatherData(forecastURL, (err, result) => {
-				if(err){
-					console.log(err);
-					res.redirect('/');
-				}else{
-					var myForecastData = result;
-					res.render('show-info', {
-						currentData: myWeatherData,
-						forecastData: myForecastData[0],
-						graphicsData: myForecastData[1]
-					});
-				}
-			});
-		}
+	const weatherData = await openWeatherProvider.getWeatherData(req.body.cityName);
+	const forecastData = await openWeatherProvider.getForecastData(req.body.cityName);
+
+	const weatherObject = new WeatherInformation(weatherData);
+	const forecastObject = new ForecastWeatherInfo(forecastData);
+
+	res.render('show-info', {
+		currentData: weatherObject.data,
+		forecastData: forecastObject.forecastData,
+		graphicsData: forecastObject.hourlyTempGraphs,
 	});
+
 };
